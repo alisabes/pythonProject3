9c92c019c0fde71015306cbd145212e6bb6faa22
Алиса <alisabes22@gmail.com>
2023-12-12 22:14:05 +0300

    инит

---
+++ main.py
@@ -0,0 +1,15 @@
+a = input()
+m = a.split()
+
+for i in m:
+    d = len(i)
+    if d % 2 == 1:
+        left = i[:d // 2]
+        right = i[d // 2 + 1:]
+        right = right[::-1]
+    else:
+        left = i[:d // 2]
+        right = i[d // 2:]
+        right = right[::-1]
+    if left == right:
+        print(i)
